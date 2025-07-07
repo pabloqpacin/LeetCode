d98ef58cae373d7496d4e120298d6abdc2912e3f
pabloqpacin <110383181+pabloqpacin@users.noreply.github.com>
2025-07-08 00:17:58 +0200

    Add hello_world.cpp

---
+++ src/hello_world.cpp
@@ -0,0 +1,16 @@
+/*
+Compilación
+$ g++ src/hello_world.cpp -o bin/hello_world
+$ ./bin/hello_world
+*/
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main(){
+  cout << "Hello World!" << endl;
+  // cout << "Hello World!" << flush;
+
+  return 0;
+}
