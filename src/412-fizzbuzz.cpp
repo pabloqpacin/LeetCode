7ec22c09d16f989ee0ebd3cd973ace36972c7197
pabloqpacin <110383181+pabloqpacin@users.noreply.github.com>
2025-07-08 01:21:39 +0200

    feat(fizzbuzz): añadir respuesta válida + código comentado para Make y ci.yml (fallaría...)

--- src/412-fizzbuzz.cpp
+++ src/412-fizzbuzz.cpp
@@ -26,11 +26,44 @@ Constraints:
 1 <= n <= 104
 */
 
+// #include <vector>
+// #include <string>
+
+// class Solution {
+// public:
+//     std::vector<std::string> fizzBuzz(int n) {
+//         std::vector<std::string> result;
+//         for (int i = 1; i <= n; i++) {
+//             if (i % 3 == 0 && i % 5 == 0) {
+//                 result.push_back("FizzBuzz");
+//             } else if (i % 3 == 0) {
+//                 result.push_back("Fizz");
+//             } else if (i % 5 == 0) {
+//                 result.push_back("Buzz");
+//             } else {
+//                 result.push_back(std::to_string(i));
+//             }
+//         }
+//         return result;
+//     }
+// };
 
 class Solution {
 public:
     vector<string> fizzBuzz(int n) {
-
+        vector<string> result;
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                result.push_back("FizzBuzz");
+            } else if (i % 3 == 0) {
+                result.push_back("Fizz");
+            } else if (i % 5 == 0) {
+                result.push_back("Buzz");
+            } else {
+                result.push_back(to_string(i));
+            }
+        }
+        return result;
     }
 };
 
